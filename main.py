0790c53f91b86cb85a48a8111c122398fce98b5a
Bram Van Genabet <bramvg@gmail.com>
2023-01-17 00:20:01 +0100

    commit -- after test working on RPI

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ app = Flask(__name__)
 
 @app.route('/P1', methods=['GET'])
 def get_energy_data():
-    # Read data from P1 energy meter
-    #data = ser.readline().decode('utf-8')
-
-    # Use regular expressions to parse data
-    #match = re.search("(?P<power>[0-9.]+) kW", data)
-    #power = float(match.group("power"))
-
     energy_data = readSerial.readP1()
     print(energy_data)
     # Return data as JSON
