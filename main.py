5ec2bcc87b11f33e5dc09aef7d6cd201ead4b1b5
Bram Van Genabet <bramvg@gmail.com>
2023-01-17 23:21:14 +0100

    cleaned up code delete testSerial

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# This script will open a REST API to publish the data from a P1 meter
+# The connectoin and parsing of the P1 meter has been modified based on a script created by Jens depuydt (https://github.com/jensdepuydt)
+# This script is created by Bram Van Genabet (https://github.com/bramvg87)
+# Instructions can be found here (https://bram.vangenabet.com/home-automation/connecting-p1-port-to-rpi)
+
+
 from readSerial import readSerial
 from flask import Flask, jsonify
 
@@ -7,7 +13,7 @@ app = Flask(__name__)
 @app.route('/P1', methods=['GET'])
 def get_energy_data():
     energy_data = readSerial.readP1()
-    print(energy_data)
+    #print(energy_data)
     # Return data as JSON
     return jsonify(energy_data)
 
